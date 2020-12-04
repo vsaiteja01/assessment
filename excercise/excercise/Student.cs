130b3342c567c72a14372f3fbf18149bcc39e628
vsaiteja01 <saitejatinkurocks@gmail.com>
2020-12-04 16:49:50 +0530

    second commit

---
+++ excercise/excercise/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace excercise
+{
+    public class Student
+    {
+        public int SID { get; set; }
+        public string SName { get; set; }
+
+        public string SDob { get; set; }
+
+        public string SContact { get; set; }
+    }
+}
